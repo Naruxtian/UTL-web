c0c07f6fd02fe6c27bc69de897ef6e846b886422
thefinn150 <xxthefinn150xx@gmail.com>
2023-10-17 13:59:02 -0600

    17/10/2023 Contactanos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import Inicio from './pages/inicio/Inicio'
-import Aspirante from './pages/aspirante/Aspirante'
-import SideMenu from './components/sideMenu/sideMenu';
-import {GiHamburgerMenu} from 'react-icons/gi'
-import './App.css'
+import { useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Inicio from "./pages/inicio/Inicio";
+import Aspirante from "./pages/aspirante/Aspirante";
+import SideMenu from "./components/sideMenu/sideMenu";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./App.css";
 
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Contactanos from "./pages/aspirante/Contactanos";
 function App() {
-  const [showNav, setShowNav] = useState(false)
-
+  const [showNav, setShowNav] = useState(false);
 
   return (
     <>
       <header>
-        <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        <GiHamburgerMenu onClick={() => setShowNav(!showNav)} />
       </header>
-      <SideMenu show={showNav}/>
-      <Routes>          
-            <Route index element={<Inicio/>} />
-            <Route path="/" element={<Inicio/>}/>
-            <Route path="/Aspirante" element={<Aspirante/>}/>
-      </Routes>
+      <SideMenu show={showNav} />
+      <Routes>
+        <Route index element={<Inicio />} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/Aspirante" element={<Aspirante />} />
 
+        <Route path="/Contactanos" element={<Contactanos />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
