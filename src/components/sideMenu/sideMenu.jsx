0cc7f29c2bb2c2d6c04424e91e2d9f482bc5df4b
Naruxtian <Naruxtian@Hotmail.com>
2023-11-07 17:50:00 -0600

    fix

--- src/components/sideMenu/sideMenu.jsx
+++ src/components/sideMenu/sideMenu.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-import { Link } from "react-router-dom";
-import logo from "../../assets/img/utlLogoWhite.png";
+import SideNav, { NavItem, NavIcon, NavText} from '@trendmicro/react-sidenav';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../assets/img/utlLogoWhite.png'
 import { AiFillHome, AiOutlineUserAdd } from "react-icons/ai";
 import { PiStudentFill } from "react-icons/pi";
 import { SiGoogleclassroom } from "react-icons/si";
@@ -11,71 +11,66 @@ import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import './sideMenu.css'
 
 const SideMenu = () => {
+
+    const navigate = useNavigate();
+
   return (
-    <SideNav
-      className="sideMenu"
-      onSelect={(selected) => {
-        console.log(selected);
-      }}
-    >
-      <SideNav.Toggle />
-      <SideNav.Nav defaultSelected="Inicio">
-        <img src={logo} alt="logo" className="menuLogo" />
+    <SideNav className="sideMenu"
+    onSelect={(selected) => {
+      console.log(selected)
+      navigate(`/${selected}`)
+    }}
+>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="Inicio" >
 
-        <Link to="/" className="MenuItm">
-          <NavItem eventKey="Inicio" className="iconoInicio">
-            <NavIcon>
-              <AiFillHome className="ic" />
-            </NavIcon>
-            <NavText></NavText>
-          </NavItem>
-        </Link>
+        <img src={logo} alt="logo" className='menuLogo'/>
 
+        <NavItem eventKey="">
+                <NavIcon>
+                    <AiFillHome className='ic'/>
+                </NavIcon>
+                <NavText>
+                    Inicio
+                </NavText>
+        </NavItem>
+        
         <NavItem eventKey="aspirante">
-          <NavIcon>
-            <AiOutlineUserAdd className="ic" />
-          </NavIcon>
-          <NavText>Soy aspirante</NavText>
-
-          <NavItem eventKey="aspirante/conoceLaUtl">
+            <NavIcon>
+                <AiOutlineUserAdd className='ic'/>
+            </NavIcon>
             <NavText>
-              <Link to="/Aspirante" className="MenuItm">
-                Conoce la UTL
-              </Link>
+                Soy aspirante
             </NavText>
-          </NavItem>
 
-          <NavItem eventKey="aspirante/ofertaEducativa">
-            <NavText>
-              <Link to="/Aspirante" className="MenuItm">
-                Oferta educativa
-              </Link>
-            </NavText>
-          </NavItem>
+            {/* <NavItem eventKey="aspirante/conoceLaUtl">
+                <NavText>
+                  <Link to="/Aspirante" className='MenuItm'>Conoce la UTL</Link>
+                </NavText>
+            </NavItem>
 
-          <NavItem eventKey="aspirante/IngresarEstudiar">
+            <NavItem eventKey="aspirante/ofertaEducativa">
             <NavText>
-              <Link to="/Aspirante" className="MenuItm">
-                Ingresa a la UTL
-              </Link>
-            </NavText>
-          </NavItem>
+                  <Link to="/Aspirante" className='MenuItm'>Oferta educativa</Link>
+                </NavText>
+            </NavItem>
+
+            <NavItem eventKey="aspirante/IngresarEstudiar">
+                <NavText>
+                    <Link to="/Aspirante" className='MenuItm'>Ingresa a la UTL</Link>
+                </NavText>
+            </NavItem> */}
         </NavItem>
 
         <NavItem eventKey="estudiante">
-          <NavIcon>
-            <PiStudentFill className="ic" />
-          </NavIcon>
-          <NavText>Soy estudiante</NavText>
-
-          <NavItem eventKey="estudiante/a">
+            <NavIcon>
+            <PiStudentFill className='ic'/>
+            </NavIcon>
             <NavText>
-              <Link to="/Estudiante" className="MenuItm">
-                Tramites Escolares
-              </Link>
+                Soy estudiante
             </NavText>
         
-            <NavItem eventKey="estudiante/a">
+            {/* <NavItem eventKey="estudiante/a">
                 <NavText>
                     <Link to="/Estudiante" className='MenuItm'>Tramites Escolares</Link>
                 </NavText>
@@ -116,7 +111,7 @@ const SideMenu = () => {
                     <Link to="/Estudiante" className='MenuItm'>Quejas y Denuncias</Link>
                 </NavText>
 
-            </NavItem>
+            </NavItem> */}
             
         </NavItem>
 
@@ -128,7 +123,7 @@ const SideMenu = () => {
                 Soy docente
             </NavText>
         
-            <NavItem eventKey="docente/a">
+            {/* <NavItem eventKey="docente/a">
                 <NavText>
                     <Link to="/Docente" className='MenuItm'>Recursos Para La Gestión Docente</Link>
                 </NavText>
@@ -156,7 +151,7 @@ const SideMenu = () => {
                 <NavText>
                     <Link to="/Docente" className='MenuItm'>Órganos Colegiados</Link>
                 </NavText>
-            </NavItem>
+            </NavItem> */}
             
         </NavItem>
 
@@ -168,7 +163,7 @@ const SideMenu = () => {
                 Soy administrativo/a
             </NavText>
         
-            <NavItem eventKey="administrativo/a">
+            {/* <NavItem eventKey="administrativo/a">
                 <NavText>
                     <Link to="/Administrativo" className='MenuItm'>Recursos Para La Gestión Administrativa</Link>
                 </NavText>
@@ -202,7 +197,7 @@ const SideMenu = () => {
                 <NavText>
                     <Link to="/Administrativo" className='MenuItm'>Seguridad Institucional</Link>
                 </NavText>
-            </NavItem>
+            </NavItem> */}
             
         </NavItem>
 
@@ -214,7 +209,7 @@ const SideMenu = () => {
                 Soy empresario/a
             </NavText>
         
-            <NavItem eventKey="empresario/a">
+            {/* <NavItem eventKey="empresario/a">
                 <NavText>
                     <Link to="/Empresario" className='MenuItm'>Conoce la UTL</Link>
                 </NavText>
@@ -242,7 +237,7 @@ const SideMenu = () => {
                 <NavText>
                     <Link to="/Empresario" className='MenuItm'>Servicios</Link>
                 </NavText>
-            </NavItem>
+            </NavItem> */}
             
         </NavItem>
 
@@ -254,7 +249,7 @@ const SideMenu = () => {
                 Soy egresado/a
             </NavText>
         
-            <NavItem eventKey="egresado/a">
+            {/* <NavItem eventKey="egresado/a">
                 <NavText>
                     <Link to="/Egresado" className='MenuItm'>Bolsa de Trabajo</Link>
                 </NavText>
@@ -276,12 +271,13 @@ const SideMenu = () => {
                 <NavText>
                     <Link to="/Egresado" className='MenuItm'>Quieres Laborar en UTL?</Link>
                 </NavText>
-            </NavItem>
+            </NavItem> */}
             
         </NavItem>
-      </SideNav.Nav>
-    </SideNav>
-  );
-};
 
-export default SideMenu;
+    </SideNav.Nav>
+  </SideNav>
+  )
+}
+
+export default SideMenu
